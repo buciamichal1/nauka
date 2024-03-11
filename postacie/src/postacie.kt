0d133038498d52dbb26932c69e5242c649c0e715
buciamichal1 <buciamichal@gmail.com>
2024-03-11 17:17:39 +0100

    Modyfikacja postacie.kt dododanie wpisywania imion

--- postacie/src/postacie.kt
+++ postacie/src/postacie.kt
@@ -20,7 +20,7 @@ open class Mag(imie:String,postac: Postac) : Postac(imie) {
             var damage_mag_czarem = round(((crit * damage_mag * wojownik.odpornosc_mag).toFloat())*100)/100
             wojownik.zycie = round(((wojownik.zycie - damage_mag_czarem).toFloat())*100)/100
             mana -= 20
-            println("Zadałeś obrazenia magiczne czarem przeciwnikowi ${wojownik.imie} równe ${damage_mag_czarem}")
+            println("${imie} zadałeś obrazenia magiczne czarem przeciwnikowi ${wojownik.imie} równe ${damage_mag_czarem}")
             println("${wojownik.imie} hp ${wojownik.zycie}")
 
     }}
@@ -28,7 +28,7 @@ open class Mag(imie:String,postac: Postac) : Postac(imie) {
     fun atak_fiz(postac: Postac, wojownik: Wojownik,crit: Float) {
         var damage_fiz_kosturem = round(((crit * damage_fiz * wojownik.odpornosc_fiz).toFloat())*100)/100
         wojownik.zycie = round(((wojownik.zycie - damage_fiz_kosturem).toFloat())*100)/100
-        println("Zadałeś obrazenia fizyczne kosturem przeciwnikowi ${wojownik.imie} równe ${damage_fiz_kosturem}")
+        println("${imie} zadałeś obrazenia fizyczne kosturem przeciwnikowi ${wojownik.imie} równe ${damage_fiz_kosturem}")
         println("${wojownik.imie} hp ${wojownik.zycie}")
     }
 }
@@ -45,7 +45,7 @@ open class Wojownik(imie:String,postac: Postac) : Postac(imie) {
             var damage_fiz_mieczem = round(((crit * damage_fiz * mag.odpornosc_fiz).toFloat())*100)/100
             mag.zycie = round(((mag.zycie - damage_fiz_mieczem).toFloat())*100)/100
             stamina -= 15
-            println("Zadałeś obrazenia fizyczne mieczem przeciwnikowi ${mag.imie} równe ${damage_fiz_mieczem}")
+            println("${imie} zadałeś obrazenia fizyczne mieczem przeciwnikowi ${mag.imie} równe ${damage_fiz_mieczem}")
             println("${mag.imie} hp ${mag.zycie}")
 
     }}
@@ -55,7 +55,7 @@ open class Wojownik(imie:String,postac: Postac) : Postac(imie) {
             var damage_fiz_tarcza = round(((crit * 0.7 * damage_fiz * mag.odpornosc_fiz).toFloat())*100)/100
             mag.zycie = round(((mag.zycie - damage_fiz_tarcza).toFloat())*100)/100
             stamina -= 10
-            println("Zadałeś obrazenia fizyczne tarczą przeciwnikowi ${mag.imie} równe ${damage_fiz_tarcza}")
+            println("${imie} zadałeś obrazenia fizyczne tarczą przeciwnikowi ${mag.imie} równe ${damage_fiz_tarcza}")
             println("${mag.imie} hp ${mag.zycie}")
 
     }}
@@ -97,34 +97,37 @@ fun start(mag:Mag,wojownik: Wojownik){
 }
 
 fun main() {
-    //var randomInt = (0..1).random()
-    var tomusPostac = Postac("Tomuś")
-    var pawelekPostac = Postac("Pawełek")
-    var tomus = Mag("Tomuś", tomusPostac)
-    var pawelek = Wojownik("Pawełek", pawelekPostac)
-    start(tomus,pawelek)
+    print("Podaj imie maga:")
+    var imiemag = readLine()!!
+    print("Podaj imie wojownik:")
+    var imiewojownik = readLine()!!
+    var magPostac = Postac(imiemag)
+    var wojownikPostac = Postac(imiewojownik)
+    var mag = Mag(imiemag, magPostac)
+    var wojownik = Wojownik(imiewojownik, wojownikPostac)
+    start(mag,wojownik)
     while (true) {
         var keyCode = System.`in`.read()
         var crit = krytyczny()
         when (keyCode){
             /*'\t'.toInt() -> {val key = System.`in`.read()
                                 when(key){*/
-                                    'w'.toInt() -> {tomus.atak_mag(tomus, pawelek,crit)}
-                                    'd'.toInt() -> {tomus.atak_fiz(tomus, pawelek,crit)}
+                                    'w'.toInt() -> {mag.atak_mag(mag,wojownik,crit)}
+                                    'd'.toInt() -> {mag.atak_fiz(mag,wojownik,crit)}
                                     /*else -> println("miss")
                                 }}
             '+'.toInt() -> {val key = System.`in`.read()
                                 when(key){*/
-                                    '8'.toInt() -> {pawelek.atak_mieczem(pawelek, tomus,crit)}
-                                    '4'.toInt() -> {pawelek.atak_tarcza(pawelek, tomus,crit)}
+                                    '8'.toInt() -> {wojownik.atak_mieczem(wojownik,mag,crit)}
+                                    '4'.toInt() -> {wojownik.atak_tarcza(wojownik,mag,crit)}
                                     /*else -> println("miss")
                                 }}*/
-            10 -> {statystyki(tomus,pawelek)}
+            10 -> {statystyki(mag,wojownik)}
             else -> {println("Zły klawisz")}
         }
-        if(pawelek.zycie < 1){println("Wygrał Tomuś")
+        if(mag.zycie < 1){println("Wygrał ${wojownik.imie}")
             break}
-        if(tomus.zycie < 1){println("Wygrał Pawełek")
+        if(wojownik.zycie < 1){println("Wygrał ${mag.imie}")
             break}
 
     }
