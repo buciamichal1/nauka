2a6fd6a6352c0d559632e665f2a1437cc28caaee
buciamichal1 <buciamichal@gmail.com>
2024-03-21 14:10:38 +0100

    Zmiana build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,19 @@
 plugins {
     kotlin("jvm") version "1.9.21"
+    id("org.springframework.boot") version "2.6.4"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
 }
-
-group = "org.example"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("org.springframework.boot:spring-boot-starter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.test {
     useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(21)
 }
